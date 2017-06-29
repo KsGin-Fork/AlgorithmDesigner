2c4d80f1f22d74177dfd0c185ff7ded76c946d71
ksgin <imqqyangfan@gmail.com>
2017-06-29 19:17:14 +0800

    Modify ZeroOnePackage with DP at 2017-6-29 by windows

--- src/DynamicProgramming/ZeroOnePackage.java
+++ src/DynamicProgramming/ZeroOnePackage.java
@@ -8,7 +8,20 @@ public class ZeroOnePackage {
         System.out.println("Hello , ZeroOnePackage");
     }
 
-    public static int Knapsack(){
+    public static int Knapsack(int c /*最大容量*/ , int[] w /*物品列表*/ , int[] s /*结果列表*/){
+        int n = w.length;
+        int[][] dp = new int[c][n];
+        int bestw = 0;
+        int curc = c;
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < c ; j++){
+                if (j < w[i]) {
+                    dp[i][j] = 0;
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
         return 0;
     }
 }
